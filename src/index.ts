178c14f31cd6fed62a2df9dc22da1e43616fb78f
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-08-08 17:43:10 -0300

    configurando archivo index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hola typescripts123')
+import "reflect-metadata";
+import app from './app';
+import { AppDataSource } from "./db";
+import {PORT} from './config'
+
+
+async function main() {
+  try {
+    await AppDataSource.initialize();
+    console.log('Database Connected...');
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
+  } catch (error) {
+    console.error(error);
+  }
+}
+main();
