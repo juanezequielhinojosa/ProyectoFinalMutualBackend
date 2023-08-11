86c592debd525ac8ecd71ede20b785bd27289677
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-08-11 13:42:38 -0300

    trabajando en las entidades Afiliado y Comercio

---
+++ src/entities/Comercio.ts
@@ -0,0 +1,37 @@
+import {Entity,Column, PrimaryGeneratedColumn,BaseEntity,CreateDateColumn,UpdateDateColumn, OneToOne, JoinColumn, OneToMany} from "typeorm";
+import { Domicilio } from "./Domicilio";
+import { Usuario } from "./Usuario";
+import { Orden } from "./Orden";
+  
+  @Entity() 
+
+  export class Comercio extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nombre: string;
+
+    @Column()
+    telefono: number
+
+    @Column()
+    cuit: number;
+  
+    @Column({ default: true })
+    active: boolean;
+  
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @OneToOne(()=>Domicilio)
+    @JoinColumn({name: 'domicilio_id'})
+    domicilio: Domicilio;
+
+    @OneToMany(() => Orden, (orden)=>orden.comercio)
+    orden: Orden[];
+
+  }
