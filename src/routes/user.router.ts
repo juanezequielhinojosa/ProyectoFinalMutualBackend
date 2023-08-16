edc2ae6ede54e0c65e625fab66ec58652f632cb8
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-08-16 17:36:30 -0300

    trabajando en el archivo de rutas de usuarios

---
+++ src/routes/user.router.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller";
+import {signIn, protectedEndpoint, refresh } from '../controllers/user.controller'
+import passport from 'passport'
+const router = Router();
+
+
+/* El código está definiendo varias rutas para manejar operaciones relacionadas con el usuario en un
+Aplicación Express.js. Aquí hay un desglose de lo que hace cada ruta: */
+router.get("/users", getUsers);
+router.get("/users/:id", getUser);
+router.post("/users/signup", createUser);
+router.delete("/users/:id", deleteUser);
+router.put("/users/:id", updateUser);
+router.delete("/users/:id", deleteUser);
+router.post('/users/signin', signIn);
+router.post('/token', refresh);
+
+
+export default router;
