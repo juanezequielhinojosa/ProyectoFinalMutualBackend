8d3e139c44fee116a98b6b76d08bb7a1e7960b80
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-08-16 17:26:16 -0300

    configurando las dependecias

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,21 @@
 import express from 'express'
-
+import morgan from 'morgan';
+import cors from 'cors';
+import userRoutes from "./routes/user.router";
+import passportMiddleware from './middlewares/passport';
+import passport from 'passport'
+import passportLocal from "passport-local";
 //configuracion
 const app = express()
+//middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(passport.initialize());
+passport.use(passportMiddleware);
+//rutas
+app.use("/api", userRoutes);
+
 
 export default app;
