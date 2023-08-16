506044b1046847006e04d81c64fe123597b83c05
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-08-16 17:32:47 -0300

    trabajando en el archivo passport.ts

--- src/middlewares/passport.ts
+++ src/middlewares/passport.ts
@@ -1,4 +1,4 @@
-import { Usuario } from "../entities/Usuario";
+import { User } from "../entities/User";
 import { Strategy, ExtractJwt, StrategyOptions } from "passport-jwt";
 import { SECRET_KEY } from "../config";
 
@@ -10,7 +10,7 @@ const opts: StrategyOptions = {
 
 export default new Strategy(opts, async (payload, done) => {
   try {
-    const user = await Usuario.findOneBy({ id_user: parseInt(payload.id) });
+    const user = await User.findOneBy({ id_user: parseInt(payload.id) });
 
     if (user) {
       return done(null, user);
