f7dc371248e2c73d4f8f34ca354818d26ae27a10
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2023-09-01 14:54:41 -0300

    trabajando en las rutas para gestionar las cuotas

---
+++ src/routes/cuota.router.ts
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import {
+  getCuotas,
+  getCuota,
+  pagarCuota,
+  deleteCuota
+ //createCuota,
+ //updateCuota
+
+} from "../controllers/cuota.controller";
+import passport from 'passport'
+
+const router = Router();
+/* Estas líneas de código definen las rutas para manejar las solicitudes HTTP relacionadas con las cuotas. Cada
+ruta está asociada con un método HTTP específico (GET, POST, PUT, DELETE) y un método HTTP correspondiente
+función del controlador. */
+router.get("/cuotas", getCuotas);
+router.get("/cuotas/:id", getCuota);
+router.put("/cuotas/:id", pagarCuota);
+router.delete("/cuotas/:id", deleteCuota);
+//router.post("/cuotas", createCuota);
+//router.put("/cuotas/:id", updateCuota);
+
+export default router;
